462eef92524039398a7270b4e407aff21a092d91
Somkiat <somkiat.p@gmail.com>
2014-03-21 13:17:09 +0700

    Refactoring test to remove duplicate code

--- src/test/java/kata/java/tennis/KataTennisTest.java
+++ src/test/java/kata/java/tennis/KataTennisTest.java
@@ -6,18 +6,17 @@ import org.junit.Test;
 
 public class KataTennisTest {
 
+	TennisGame tennisGame = new TennisGame("Player A", "Player B");
+	
 	@Test
 	public void newGameShouldReturnLoveAll() throws Exception {
-		TennisGame tennisGame = new TennisGame("Player A", "Player B");
 		String score = tennisGame.getScore();
 		assertEquals("Love all", score);
 	}
 
 	@Test
 	public void playerAWinShouldReturnFifteenLove() throws Exception {
-		TennisGame tennisGame = new TennisGame("Player A", "Player B");
         tennisGame.playerScore(15, 0);
-
 		String score = tennisGame.getScore();
 		assertEquals("Fifteen Love", score);
 	}
