1b1b2e0701d758e37cb2ff37d960667d64c54cae
karan <bomb0069@gmail.com>
2014-03-21 12:34:06 +0700

    solved TestCase playerAWinShouldReturnFifteenLove

--- src/main/java/kata/java/tennis/TennisGame.java
+++ src/main/java/kata/java/tennis/TennisGame.java
@@ -2,10 +2,19 @@ package kata.java.tennis;
 
 public class TennisGame {
 
+	Integer playerAScore;
+
     public TennisGame(String playerA, String playerB){
+    	playerAScore = 0;
+    }
+
+    public void playerScore(Integer playerAScore, Integer playerBScore) {
+    	this.playerAScore =  playerAScore;
     }
 
     public String getScore(){
-        return "Love all";
+    	if (this.playerAScore == 0)
+        	return "Love all";
+        return "Fifteen Love";
     }
 }
