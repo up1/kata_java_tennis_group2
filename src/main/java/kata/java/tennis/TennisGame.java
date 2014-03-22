f56180c91c04fdfd0eed046a7e5de63f9ddf541c
Somkiat <somkiat.p@gmail.com>
2014-03-22 07:43:23 +0700

    Formatting code

--- src/main/java/kata/java/tennis/TennisGame.java
+++ src/main/java/kata/java/tennis/TennisGame.java
@@ -3,21 +3,21 @@ package kata.java.tennis;
 public class TennisGame {
 
 	int playerAScore = 0;
-    int playerBScore = 0;
+	int playerBScore = 0;
 
-    public TennisGame(String playerA, String playerB){
-    }
+	public TennisGame(String playerA, String playerB) {
+	}
 
-    public void playerScore(int playerAScore, int playerBScore) {
-    	this.playerAScore += playerAScore;
-        this.playerBScore += playerBScore;
-    }
+	public void playerScore(int playerAScore, int playerBScore) {
+		this.playerAScore += playerAScore;
+		this.playerBScore += playerBScore;
+	}
 
-    public String getScore(){
-        if (this.playerAScore > 0 && this.playerBScore > 0)
-            return "Fifteen all";
-    	if (this.playerAScore > 0)
-            return "Fifteen Love";
-       	return "Love all";
-    }
+	public String getScore() {
+		if (this.playerAScore > 0 && this.playerBScore > 0)
+			return "Fifteen all";
+		if (this.playerAScore > 0)
+			return "Fifteen Love";
+		return "Love all";
+	}
 }
