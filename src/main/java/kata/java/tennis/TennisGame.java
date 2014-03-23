ad332b7644d22f9ea609be4f0464c78a2822b2ae
armeo <arm@prontomarketing.com>
2014-03-23 22:49:51 +0700

    solve bothPlayerDrawWithFifteen and add one test fail

--- src/main/java/kata/java/tennis/TennisGame.java
+++ src/main/java/kata/java/tennis/TennisGame.java
@@ -2,19 +2,22 @@ package kata.java.tennis;
 
 public class TennisGame {
 
-	Integer playerAScore;
+	private int playerAScore = 0;
+	private int playerBScore = 0;
 
     public TennisGame(String playerA, String playerB){
-    	playerAScore = 0;
     }
 
-    public void playerScore(Integer playerAScore, Integer playerBScore) {
-    	this.playerAScore =  playerAScore;
+    public void playerScore(int playerAScore, int playerBScore) {
+    	this.playerAScore +=  playerAScore;
+    	this.playerBScore +=  playerBScore;
     }
 
     public String getScore(){
-    	if (this.playerAScore == 0)
+    	if (this.playerAScore == 0 && this.playerBScore == 0)
         	return "Love all";
+        else if (this.playerAScore == 15 && this.playerBScore == 15)
+            return "Fifteen all";
         return "Fifteen Love";
     }
 }
--- src/test/java/kata/java/tennis/KataTennisTest.java
+++ src/test/java/kata/java/tennis/KataTennisTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class KataTennisTest {
 
 	TennisGame tennisGame = new TennisGame("Player A", "Player B");
-	
+
 	@Test
 	public void newGameShouldReturnLoveAll() throws Exception {
 		String score = tennisGame.getScore();
@@ -15,17 +15,24 @@ public class KataTennisTest {
 	}
 
 	@Test
-	public void playerAWinShouldReturnFifteenLove() throws Exception {
+	public void playerAWinFirstBallShouldReturnFifteenLove() throws Exception {
         tennisGame.playerScore(15, 0);
 		String score = tennisGame.getScore();
 		assertEquals("Fifteen Love", score);
 	}
-	
+
 	@Test
 	public void bothPlayerDrawWithFifteenShouldReturnFifteenAll() throws Exception {
 		tennisGame.playerScore(15, 0);
 		tennisGame.playerScore(0, 15);
 		String score = tennisGame.getScore();
 		assertEquals("Fifteen all", score);
+	}
+
+	@Test
+	public void playerAWinSecondBallShouldReturnThirtyLove() throws Exception {
+        tennisGame.playerScore(30, 0);
+		String score = tennisGame.getScore();
+		assertEquals("Thirty Love", score);
 	}
 }
